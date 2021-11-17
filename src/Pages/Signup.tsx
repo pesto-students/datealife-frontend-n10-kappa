d5ed0a41d4040a27311c90808cb7c2b7651bb1b4
Veer auro <techie.vigneshramesh@gmail.com>
2021-11-17 18:55:02 +0530

    New routes added, signup and singing page (#8)
    * Basic MUI theme setup
    
    * Basic routes setup react router
    
    * Folder restructured
    
    * Dancing script font added
    
    * Logo added
    
    * Routes updated. Singup route added
    
    * Login page added
    
    Co-authored-by: vignesh <r.vignesh149@gmail.com>

---
+++ src/Pages/Signup.tsx
@@ -0,0 +1,46 @@
+import DateALifeLogo from "../assets/images/logoDateALife.png";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import GoogleIcon from "@mui/icons-material/Google";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import {  Div,
+        LogoDiv,
+        LogoImg,
+        LogoText,
+        PrivacyText,
+        FooterText } from "../assets/styles/Styles";
+import { SignupButton } from "../assets/styles/Button";
+
+const SignUp = () : JSX.Element  => {
+    return (
+        <Div>
+            <LogoDiv>
+                <LogoImg src={DateALifeLogo}  alt="logo" />
+            </LogoDiv>
+            <LogoText>Date A Life</LogoText>
+
+            <PrivacyText>
+                By clicking Log In, you agree with our Terms. Learn how we process your data in our <a href="#">Privacy Policy</a> and Cookies Policy.
+            </PrivacyText>
+
+            <SignupButton variant="contained" startIcon={<GoogleIcon color="warning" /> } color="inherit">
+                Signup with google
+            </SignupButton>
+
+            <SignupButton variant="contained" startIcon={<FacebookRoundedIcon color="info" fontSize="large" /> } color="inherit">
+                Signup with facebook
+            </SignupButton>
+
+            <SignupButton variant="contained" startIcon={<LocalPhoneIcon color="secondary" />} color="inherit">
+                Signup with number
+            </SignupButton>
+
+            <FooterText>
+                Have an account? <strong><a href="/login">Login</a></strong>
+            </FooterText>
+        </Div>
+    );
+};
+
+export default SignUp;
+
+
