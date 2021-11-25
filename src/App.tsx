14717536ad1ad8105d866a3bfe08227c4239cde0
Anooj Agarwal <mail2anoojagarwal@gmail.com>
2021-11-25 16:48:18 +0530

    feat: add chat component and its dependencies (#28)
    * feat: add chat component and its dependencies
    
    * fix: add package-lock file
    
    Co-authored-by: ANOOJ AGARWAL <Anooj.AGARWAL@icims.com>

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import Listings from "./pages/Listing";
 import Learning from "./pages/Learning";
 import Matchmaking from "./pages/Matchmaking";
 import EditProfile from "./pages/EditProfile";
+import Chat from "./pages/Chat";
 import EditAboutUs from "./pages/EditAboutUs";
 
 const App = (): JSX.Element => {
@@ -38,7 +39,7 @@ const App = (): JSX.Element => {
             <Route path="/user/profile/editProfile/editPicture" element={<EditPictures />} />
             <Route path="/user/profile/:id" element={<Home />} />
             <Route path="/learning" element={<Learning />} />
-            <Route path="/chatting" element={<Home />} />
+            <Route path="/chatting" element={<Chat />} />
             <Route path="/home" element={<Home />} />
             <Route path="/likes" element={<Listings />} />
             <Route path="/user/profile/editProfile/editAbout" element={<EditAboutUs />} />
---
+++ src/third-party/comet-chat/consts.js
@@ -0,0 +1,6 @@
+export const COMETCHAT_CONSTANTS = {
+    APP_ID: "19701141445ba4e6",
+    REGION: "us",
+    AUTH_KEY: "fe943a10efca9f22429b8d7330694efe78f810dc",
+    TEST_USER_UUID: "superhero2",
+};
