ce93b69fbc56f8db21876dc9646f72d7914e0133
vignesh <r.vignesh149@gmail.com>
2021-11-26 15:20:33 +0530

    Restructured routes component and app.tsx loading

---
+++ src/routes.ts
@@ -0,0 +1,113 @@
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Interests from "./pages/Interests";
+import EditInterests from "./pages/EditInterests";
+import EditPictures from "./pages/EditPictures";
+import Name from "./pages/Name";
+import DOB from "./pages/DOB";
+import IdentifyAs from "./pages/Identify";
+import EditIdentifyAs from "./pages/EditIdentify";
+import Number from "./pages/Number";
+import OTP from "./pages/OTP";
+import Listings from "./pages/Listing";
+import Learning from "./pages/Learning";
+import Matchmaking from "./pages/Matchmaking";
+import EditProfile from "./pages/EditProfile";
+import Chat from "./pages/Chat";
+import EditAboutUs from "./pages/EditAboutUs";
+
+const routes: RoutesType[] = [
+    {
+        pathname: "/signup",
+        component: Signup
+    },
+    {
+        pathname: "/login",
+        component: Login
+    },
+    {
+        pathname: "/user/name",
+        component: Name
+    },
+    {
+        pathname: "/user/identify",
+        component: IdentifyAs
+    },
+    {
+        pathname: "/signup/number",
+        component: Number
+    },
+    {
+        pathname: "/signup/otp",
+        component: OTP
+    },
+    {
+        pathname: "/user/dob",
+        component: DOB
+    },
+    {
+        pathname: "/user/interests",
+        component: Interests
+    },
+    {
+        pathname: "/matchmaking",
+        component: Matchmaking
+    },
+    {
+        pathname: "/user/dob",
+        component: DOB
+    },
+    {
+        pathname: "/user/picture",
+        component: Home
+    },
+    {
+        pathname: "/user/profile/editProfile",
+        component: EditProfile
+    },
+    {
+        pathname: "/user/profile/:id/editProfile/editPicture",
+        component: EditPictures
+    },
+    {
+        pathname: "/user/profile/editProfile/editPicture",
+        component: EditPictures,
+    },
+    {
+        pathname: "/learning",
+        component: Learning
+    },
+    {
+        pathname: "/chatting",
+        component: Chat
+    },
+    {
+        pathname: "/home",
+        component: Matchmaking,
+    },
+    {
+        pathname: "/likes",
+        component: Listings
+    },
+    {
+        pathname: "/user/profile/editProfile/editAbout",
+        component: EditAboutUs
+    },
+    {
+        pathname: "/user/profile/editProfile/editInterests",
+        component: EditInterests,
+    },
+    {
+        pathname: "/user/profile/editProfile/editIdentify",
+        component: EditIdentifyAs,
+    }
+];
+
+
+type RoutesType = {
+    pathname: string,
+    component: any
+};
+
+export default routes;
